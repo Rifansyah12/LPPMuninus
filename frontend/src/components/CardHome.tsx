1c5d996bd4abb4005beee2bfef49bec45ee4701c
Inal Mahpud <inalmahpud1@gmail.com>
2024-10-03 21:02:50 +0700

    feat: homePage

---
+++ frontend/src/components/CardHome.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export function CardHome({ backgroundColor = "#1C532A", img = "/research.png" }) {
+  return (
+    <div className="w-[358px] flex shadow-md rounded-md">
+      <div
+        className={`w-[179px] h-[179px] bg-[${backgroundColor}] flex justify-center items-center shadow-md rounded-l-md`}
+      >
+        <Image src={img} alt={"LPPM"} width={117} height={117} />
+      </div>
+      <div className="bg-white w-[179px] h-[179px] flex justify-center flex-col pl-2 shadow-md rounded-r-md">
+        <h3 className="text-[#3C3C3C] font-bold text-lg">Lorem</h3>
+        <p className="text-[#3C3C3C]">
+          Lorem ipsum dolor sit amet, amet consectetur adipiscing elit. Nullam
+          eget ullamcorper nisl....
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/pages/main/index.tsx
@@ -0,0 +1,45 @@
+import { CardHome } from "@/components/CardHome";
+import Navbar from "@/components/Navbar";
+import Head from "next/head";
+export default function Main() {
+  return (
+    <div className="">
+      <Head>
+        <title>Beranda</title>
+      </Head>
+      <Navbar />
+      <div className="bg-[#F4F6F9] w-full min-h-screen px-16 py-7">
+        <div className="text-end">
+          <a href="https://lppm.uninus.ac.id">
+            <h2 className="text-blue-300">FAQS</h2>
+          </a>
+          <h2 className="text-black font-bold text-2xl py-2">BERANDA</h2>
+        </div>
+
+        <div className="bg-white w-full px-7 py-7 my-2">
+          <div className="flex items-center gap-1">
+            <h1 className="text-3xl font-bold text-[#3C3C3C]">SIMPPM</h1>
+            <div className="block h-[7px] bg-[#FFCC00] w-[176px]"></div>
+          </div>
+          <p className="text-[#3C3C3C] font-semibold text-lg">
+            Sistem Informasi Manajemen, Penelitian dan Pengabdian Kepada
+            Masyarakat Universitas Islam Nusantara
+          </p>
+        </div>
+
+        <div className="flex justify-center items-center flex-wrap gap-4 my-[4rem]">
+          <CardHome />
+          <CardHome backgroundColor="#FFCC00" img="/humans.png" />
+          <CardHome backgroundColor="#1E73BE" img="/box.png" />
+        </div>
+
+        <footer>
+          <span className="h-[2px] block w-full bg-[#D9D9D9] mt-[9rem] mb-4"></span>
+          <p className="text-md text-[#3C3C3C]">
+            COPYRIGHT 2024 - LPPM UNIVERSITAS ISLAM NUSANTARA
+          </p>
+        </footer>
+      </div>
+    </div>
+  );
+}
